70d25af250a65ba832d240d1d268d1d05fa9789c
Chetan Raut <Chetan.raut2009@gmail.com>
2024-01-31 15:57:35 +0530

    Update AccessDataMembers.java

--- P1/AccessDataMembers.java
+++ P1/AccessDataMembers.java
@@ -1,15 +1,13 @@
-public class AccessDataDemo {
+// Create a class named Student
+class Student {
+    int studentID; // Data member to store student ID
+    String studentName; // Data member to store student name
+}
 
-    // Create a class named Student
-    class Student {
-        int studentID; // Data member to store student ID
-        String studentName; // Data member to store student name
-    }
-
+public class Main {
     public static void main(String[] args) {
         // Create an object of the Student class
-        AccessDataDemo accessDataDemo = new AccessDataDemo();
-        Student studentObject = accessDataDemo.new Student();
+        Student studentObject = new Student();
 
         // Access and set data members of the Student object
         studentObject.studentID = 101;
@@ -19,4 +17,4 @@ public class AccessDataDemo {
         System.out.println("Student ID: " + studentObject.studentID);
         System.out.println("Student Name: " + studentObject.studentName);
     }
-  }
+}
